84b1379ba92f37d59bdeab03e4ef00da301cb9be
Linkkader <linkkader14@gmail.com>
2021-01-06 15:33:31 +0100

    z

--- MyWaifuApi/settings.py
+++ MyWaifuApi/settings.py
@@ -1,3 +1,5 @@
+import django_heroku
+
 """
 Django settings for MyWaifuApi project.
 
@@ -123,3 +125,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+django_heroku.settings(locals())
--- myapi/urls.py
+++ myapi/urls.py
@@ -5,8 +5,6 @@ from . import views
 router = routers.DefaultRouter()
 router.register(r'waifu',views.waifuViewSet,basename="queryset")
 router.register(r'image',views.imageViewSet,basename="queryset")
-
-
 urlpatterns = [
     path('',include(router.urls)),
     path('api-auth/',include('rest_framework.urls',
